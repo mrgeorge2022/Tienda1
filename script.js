ee5b8f136ca63c2ab80306481b5e5461decbe6b1
mrgeorge2022 <mr.georgesince2022@gmail.com>
2025-02-27 16:31:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -872,7 +872,7 @@ const horariosTienda = [
   { dia: 1, horaApertura: 18, horaCierre: 24 },  // Lunes 
   { dia: 2, horaApertura: 18, horaCierre: 24 },  // Martes
   { dia: 3, horaApertura: null, horaCierre: null},  // Miércoles - cerrdado
-  { dia: 4, horaApertura: 16, horaCierre: 24 },  // Jueves -prueba
+  { dia: 4, horaApertura: 18, horaCierre: 24 },  // Jueves 
   { dia: 5, horaApertura: 18, horaCierre: 24 },  // Viernes
   { dia: 6, horaApertura: 18, horaCierre: 24 },  // Sábado
 ];
